4f9269d6f6d1d5a82f9c5bf38655380b12feaa31
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-04-21 13:40:28 -0400

    dictionary implemented and bugs worked out

--- War/war_version03.py
+++ War/war_version03.py
@@ -9,13 +9,30 @@ Skills required: Counting and card values
 """
 
 
-# version two
-# if draw I declare war and compare 
+# version three
+
+# 1. make draw a funciton
+# 2. introduce dictionary to implement face cards 
 
 # we have four suits but for the game suits don't matter
 # the deck has 52 cards
 # each suit has 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
 # where J:11, Q:12, K:13, A:14
+dict = { 
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14,
+}
 
 import random
 
@@ -25,13 +42,13 @@ import random
 # C: clubs
 # S: spades
 
-deck1 = []
+deck = []
 suit = ['D','H', 'S', 'C']
 cards = ['2','3','4','5','6','7','8','9','10', 'J', 'Q', 'K', 'A']
 for symbol in suit:
     for card in cards:
         # print(symbol + card)
-        deck1.append(symbol+card)
+        deck.append(symbol+card)
 # dictionary to match card to value
 
 
@@ -40,7 +57,7 @@ for symbol in suit:
 # store deck1[1:]
 
 # create a deck of cards with no suits
-deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
+# deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
 
 # shuffle a deck
 random.shuffle(deck) 
@@ -51,7 +68,6 @@ player2_hand = deck[len(deck)//2:]
 
 print(len(deck))
 print(deck)
-print(deck1)
 player1_score = 0
 player2_score = 0
 draw_counter = 0
@@ -60,38 +76,50 @@ i = 0
 
 while player1_hand and player2_hand:
 
-    # pop one card each
-    player1_card = player1_hand.pop(0)
-    player2_card = player2_hand.pop(0)
+    print(player1_hand)
+    print(player2_hand)
+
+    # pop one card eachprint(deck1)
+    # change player1_card to player1_str
+    player1_strA = str(player1_hand.pop(0))
+    player2_strA = str(player2_hand.pop(0))
+    # this catches the empty pop
+    player1_str = player1_strA[1:]
+    player2_str = player2_strA[1:]
+    
+    player1_card = dict[player1_str]
+    player2_card = dict[player2_str]
+    # where ever pop is compare need to paste this
+
 
     print("~" * 40)
     print(f"Round {i}")
     i += 1
     input("\n1, 2, 3... War : Hit Enter")
-    print(f"Player One : {player1_card}   and  Player Two : {player2_card}")
+    print(f"Player One : {player1_strA}   and  Player Two : {player2_strA}")
     
     # compare_cards
     if player1_card > player2_card:
         print("Player One Wins!")
         player1_score +=1
         # winner collects spoils
-        player1_hand.extend([player1_card, player2_card])
+        player1_hand.extend([player1_strA, player2_strA])
     elif player2_card > player1_card:
         print("Player Two Wins!")
         player2_score +=1
         # winner collects spoils
-        player2_hand.extend([player1_card, player2_card])
+        player2_hand.extend([player1_strA, player2_strA])
     else:
         print("It's a Draw")
         draw_counter += 1
 
         # check which player has enough cards to play draw hand
         # check len(player1_hand < 4)
-        if len(player1_hand) <= 4:
+        if len(player1_hand) < 4:
             #player 2 wins this round
             print("\n\n\n")
             break
-        if len(player2_hand) <= 4:
+        if len(player2_hand) < 4:
             # player 1 wins this round
             print("\n\n\n")
             break
@@ -103,16 +131,26 @@ while player1_hand and player2_hand:
         p1_1 = player1_hand.pop(0)
         p1_2 = player1_hand.pop(0)
         p1_3 = player1_hand.pop(0)
-        player1_card = player1_hand.pop(0)
+        player1_strA = str(player1_hand.pop(0))
 
         p2_1 = player2_hand.pop(0)
         p2_2 = player2_hand.pop(0)
         p2_3 = player2_hand.pop(0)
-        player2_card = player2_hand.pop(0)
+        player2_strA = str(player2_hand.pop(0))
 
-        spoils = [p1_1, p1_2, p1_3, player1_card, p2_1, p2_2, p2_3, player2_card]
+        # this catches the empty pop
+        player1_str = player1_strA[1:]
+        player2_str = player2_strA[1:]
+        
+        player1_card = dict[player1_str]
+        player2_card = dict[player2_str]
+        # where ever pop is compare need to paste this
+
+
+
+        spoils = [p1_1, p1_2, p1_3, player1_strA, p2_1, p2_2, p2_3, player2_strA]
         # compare_card
-        print(f"Player One : {player1_card}   and  Player Two : {player2_card}")
+        print(f"Player One : {player1_strA}   and  Player Two : {player2_strA}")
         if player1_card > player2_card:
             print("Player One Wins!")
             player1_score +=1
